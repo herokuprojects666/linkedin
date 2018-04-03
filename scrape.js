4f79b84d9616d47a45be05ce005bcd0c6cc34a8d
= <herokuprojects666@gmail.com>
2018-04-02 21:07:16 -0700

    updating scraper to the point where we can now login.

--- scrape.js
+++ scrape.js
@@ -1,6 +1,17 @@
-var casper = require('casper').create();
-var count = 5;
+var fs = require('fs');
+var currentFile = require('system').args[3];
+var casper = require('casper').create({
+		pageSettings: {
+		userAgent: 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'
+	}});
 
+var currentPath = fs.absolute(currentFile).split('/')
+.filter(function(path, index, array) { return index < array.length - 1})
+.join('/') + '/';
+
+var maxContactCount = 5;
+
+/** User supplied arguments */
 var contactCount = casper.cli.get('c')
 var url = casper.cli.get('url')
 var username = casper.cli.get('user')
@@ -19,6 +30,40 @@ if (contactCount) {
 	count = contactCount
 }
 
-casper.start(url, function() {})
+/** @todo: account for captcha */
+casper.start("https://www.linkedin.com/uas/login", function() {
+	/** Wait for form inputs to be visible */
+	this.waitFor(function() {
+		return this.evaluate(function() {
+			return $('.form-email').find('input').length
+		})
+	})
+	/** Send the user supplied values to the corresponding inputs */
+	this.then(function() {
+		this.sendKeys('.form-email input', username)
+		this.sendKeys('.form-password input', password)
+		this.sendKeys('.form-password input', casper.page.event.key.Enter)
+	})
+
+	/** Linkedin doesn't like our UA so wait until it tells us our browser sucks */
+	this.waitFor(function() {
+		return this.evaluate(function() {
+			return document.querySelector("a[title='continue anyway']").title == 'continue anyway'
+		})
+	})
+
+	/** Grab the href attribute for the funky browser detection and redirect to that ourselves */
+	this.then(function() {
+		var continueAnyway = this.getElementsInfo("a[title='continue anyway']")[0]
+		this.thenOpen(continueAnyway.attributes.href)
+	})
+
+	/** Wait for the official login url */
+	this.waitFor(function() {
+		return this.getCurrentUrl() == 'https://www.linkedin.com/feed/'
+	})
+
+	/** Tada we are now logged in! target the url provided in the script */
+})
 
 casper.run();
